afc240cb886711c7b8b5384eb6eb160c1ba4f5c3
wook <ifol1129@gmail.com>
2022-10-07 11:08:56 +0900

    add_update_delete

--- reviews/views.py
+++ reviews/views.py
@@ -23,3 +23,34 @@ def create(request):
         'review_form': review_form,
     }
     return render(request, 'reviews/create.html', context=context)
+
+def detail(request,pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        "review":review
+    }
+    return render(request, "reviews/detail.html", context)
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+    review_form = ReviewForm(instance=review)
+    
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, instance=review)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect('reviews:index')
+    
+    context ={
+        "review_form": review_form
+    }
+    return render(request, "reviews/update.html", context)
+
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+    if request.method == "POST":
+        review.delete()
+        return redirect("reviews:index")
+
+    return render(request, "reviews/delete.html")
+    
